f2293571af29e54f45a73227a140880a29f73bb5
minhit6505 <minhit6505@gmail.com>
2025-04-24 12:27:41 +0700

    commited

--- config/jwt.js
+++ config/jwt.js
@@ -1,4 +1,4 @@
 module.exports = {
-  accessTokenExp: process.env.JWT_ACCESS_EXPIRES_IN || "20s",
+  accessTokenExp: process.env.JWT_ACCESS_EXPIRES_IN || "1d",
   refreshTokenExp: process.env.JWT_REFRESH_EXPIRES_IN || "1y",
 };
